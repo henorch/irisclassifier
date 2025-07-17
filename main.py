d08678766dec29e24d8ecbe4aa1961957f35b71f
henorch <henorch01@gmail.com>
2025-07-17 05:57:24 +0100

    ready for container

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template,request
+import joblib
 
 
 app = Flask("__name__");
+model = joblib.load("./models/irisflower.pkl")
 
 
 @app.route('/')
 def home():
-    return {"home":"Welcome home"}
+    return render_template("./predict.html")
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get data from form
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width = float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width = float(request.form['petal_width'])
+
+    features = [sepal_length, sepal_width, petal_length, petal_width]
+
+    prediction = model.predict([features])
+
+    return render_template("./predict.html", prediction=prediction[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
